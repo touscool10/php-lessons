452a9128e3c20bfdd4a5bc69c671dff07707593b
Sètondé T. Crespin Nougbodohoue <touscool10@gmail.com>
2021-10-18 10:24:43 -0300

    Finalizei_Seções_14&15_Curso I

--- unidade14/destino.php
+++ unidade14/destino.php
@@ -6,5 +6,14 @@
     </head>
 
     <body> 
+        <?php
+            
+            $nome = isset($_POST["nome"]) ? $_POST["nome"] : "Sem definição"."<br>";
+            $email = isset($_POST["email"]) ? $_POST["email"] : "Sem definição"."<br>";
+            echo "Nome: ". $nome."<br>";
+            echo "E-mail: ". $email."<br>";
+
+
+        ?>
     </body>
 </html>
